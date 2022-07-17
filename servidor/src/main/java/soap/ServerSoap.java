44ea9b591992b0d9874c2486bc5a2c827305a54b
matheusinit <matheus.oliveira.s@protonmail.com>
2022-07-17 20:01:23 -0300

    compare strings at atualizarLivro

--- servidor/src/main/java/soap/ServerSoap.java
+++ servidor/src/main/java/soap/ServerSoap.java
@@ -1,33 +1,48 @@
 package soap;
 
+import org.apache.commons.lang3.StringUtils;
+
 import javax.jws.WebService;
-import java.util.List;
-import java.util.Vector;
+import java.util.Objects;
 
 @WebService(endpointInterface = "soap.Biblioteca")
 public class ServerSoap implements Biblioteca {
-    List<Livro> livros = new Vector<>();
+    Livro[] livros = new Livro[1];
+    int size = 0;
 
     public void adicionarLivro(String name) {
         System.out.println("Adicionar livro");
 
         Livro livro = new Livro(name);
 
-        livros.add(livro);
+        if (livros.length == size) {
+            Livro[] novoLivrosArray = new Livro[size + 1];
+
+            for (int i = 0; i < livros.length; i++) {
+                novoLivrosArray[i] = livros[i];
+            }
+
+            livros = novoLivrosArray;
+        }
+
+
+        livros[size] = livro;
+
+
+        size++;
     }
 
     public Livro[] listarLivros() {
-        return (Livro[]) livros.toArray();
+        return livros;
     }
 
     public Livro atualizarLivro(String nome, String novoNome) {
-        Livro livro = livros.get(0);
+        Livro livro = livros[0];
 
-        for (Livro item : livros) {
-            if (item.nome == nome) {
-                item.nome = nome;
-
-                livro = item;
+        for (int i = 0; i < livros.length; i++) {
+            if (livros[i].nome.equals(nome)) {
+                livros[i].nome = novoNome;
+                livro = livros[i];
             }
         }
 
