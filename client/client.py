345eae3b235feca222641adc74d64a5791c1ca89
Felipe Rocha <felipedsousarocha@gmail.com>
2022-07-17 21:47:17 +0000

    Client em Python

--- client/client.py
+++ client/client.py
@@ -1,7 +1,36 @@
 from zeep import Client
 
-client = Client('')
-result = client.service.ConvertSpeed(
-    100, 'KmPorhora', 'milhasPorhora')
+class Livro:
+    pass
 
-assert result == 62.137
+def novoLivro():
+    ret = Livro()
+    ret.nome = input('Digite o nome do livro: ')
+    return ret
+
+def main():
+    client = Client('http://127.0.0.1:5000/biblioteca?wsdl')
+    print('### Biblioteca de Livros')
+    while True:
+        print('\nMENU:')
+        print('1) Inserir Livro')
+        print('2|) Listar Livros')
+        print('0) Sair')
+        op = input('> ')
+        if op == '1':
+            livro = novoLivro()
+            client.service.adicionarLivro(
+                livro.nome
+            )
+        elif op == '2':
+            ls = client.service.listarLivros()
+            for livro in ls:
+                print('')
+                print('Nome do livro: ', livro.nome)
+        elif op == '0':
+            break
+        else:
+            print('Opcao invalida!')
+
+if __name__ == '__main__':
+    main()
